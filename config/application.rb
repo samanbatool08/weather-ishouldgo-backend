60a3756b64efac4bb075fff7bd85d87327d946a9
Your Name <samanbatool08@gmail.com>
2020-07-28 15:24:44 -0400

    remove config files

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,9 @@ module WeatherApplication
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # Add node_modules to assets paths
-    config.assets.paths << Rails.root.join("node_modules")
+    
+    # # Add node_modules to assets paths
+    # config.assets.paths << Rails.root.join("node_modules")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  # Fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # # Fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
